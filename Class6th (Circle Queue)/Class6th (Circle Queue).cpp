f7a803cbaba3f34c55e1161a1456a9031187c4e8
kitsunera1138 <kitsunera1138@gmail.com>
2024-06-19 19:46:37 +0900

    (2024.6.19) 문자열
    circle queue vector

---
+++ Class6th (Circle Queue)/Class6th (Circle Queue).cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+using namespace std;
+
+#define SIZE 5
+
+template <typename T>
+class CircleQueue {
+private:
+	int front;
+	int rear;
+	int size;
+
+	T container[SIZE];
+public:
+
+	CircleQueue() {
+		size = 0;
+		front = SIZE - 1;
+		rear = SIZE - 1;
+
+		for (int i = 0;i < SIZE;i++) {
+			container[i] = NULL;
+		}
+	}
+
+	void Push(T data) {
+
+		if (front == (rear + 1) % SIZE) { //공간X
+			cout << "CircleQueue Overflow" << endl;
+		}
+		else {
+			rear = (rear + 1) % SIZE;//0번째 인덱스 들어감
+			container[rear] = data;
+
+			size++;
+		}
+
+		//ㅁㅁX
+		//if (front == rear) { //데이터 0개
+
+		//	if (rear >= SIZE - 1) {
+		//		rear = 0;
+		//		container[0] = data;
+		//	}
+		//}
+		//else {
+		//	if (rear >= SIZE - 1) {
+		//		rear = 0;
+		//		container[0] = data;
+		//	}
+		//	else {
+		//		container[rear++] = data;
+		//	}
+		//}
+	}
+
+	bool Empty() {
+
+		if (front == rear) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	void Pop() {
+
+		if (Empty()) {
+			cout << "CircleQueue is Empty" << endl;
+		}
+		else { //비어있지 않다면
+			front = (front + 1) % SIZE;
+			container[front] = NULL;
+
+			size--;
+		}
+
+		//ㅁㅁㅁ
+		//if (front >= SIZE -1) {
+		//	front = 0;
+		//	container[front] = NULL;
+		//}
+		//else {
+		//	container[++front] = NULL;
+		//}
+	}
+
+	int& Size() {
+		return size;
+	}
+	T& Front() {
+		return container[(front + 1) % SIZE];
+	}
+
+	void Show() {
+		for (int i = 0;i < SIZE - 1;i++) {
+			cout << container[i] << endl;
+		}
+	}
+};
+
+int main()
+{
+	/*
+	CircleQueue<int> circleQueue;
+	circleQueue.Push(10);
+	circleQueue.Push(20);
+	circleQueue.Push(30);
+	circleQueue.Push(40);
+
+	circleQueue.Pop();
+	circleQueue.Pop();
+
+	circleQueue.Show();
+	*/
+	CircleQueue<char> circleQueue; //size 5로 잡아서 4개만 Push 가능 
+	//원형 큐는 한칸은 비워놓음
+	circleQueue.Push('A');
+	circleQueue.Push('B');
+	circleQueue.Push('C');
+	circleQueue.Push('D');
+
+	while (circleQueue.Empty() == false) {
+		cout << circleQueue.Front() << endl;
+		circleQueue.Pop();
+	}
+
+	circleQueue.Push('E');
+	circleQueue.Push('F');
+
+	cout << endl;
+	while (circleQueue.Empty() == false) {
+		cout << circleQueue.Front() << endl;
+		circleQueue.Pop();
+	}
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,67 +1,86 @@
 ﻿#include <iostream>
+
 using namespace std;
 
-#define SIZE 5
-
 template <typename T>
-class CircleQueue {
+class Vector {
 private:
-	int front;
-	int rear;
 	int size;
+	int capacity;
 
-	T container[SIZE];
+	T* container; //크기가 변함
 public:
-
-	CircleQueue() {
+	Vector() {
 		size = 0;
-		front = SIZE - 1;
-		rear = SIZE - 1;
+		capacity = 0;
+		container = nullptr;
+	}
 
-		for (int i = 0;i < SIZE;i++) {
-			container[i] = NULL;
+	void Resize(int newSize) {
+		//1.capacity에 새로운 size 값을 저장합니다.
+		capacity = newSize;
+
+		//2.새로운 포인터 변수를 생성해서  새롭게 
+		// 만들어진 메모리 공간을 가리키도록 합니다.
+		T* newcontainer = new T[capacity]; //T* potiner = new T[capacity];
+
+		//3. 새로운 메모리 공간의 값을 초기호 합니다.
+		for (int i = 0;i < capacity;i++) {
+			newcontainer[i] = NULL;
+		}
+
+		//4.기존 배열에 있는 값을 복사해서 새로운 배열에 넣어 줍니다.
+		for (int i = 0;i < size;i++) {
+			newcontainer[i] = container[i];
+		}
+
+		//5. 기존 배열의 메모리 해제합니다.
+		//주의할 점 사이즈가 아예없을 때 배열이 할당안되었을때 할당 해제하면 안됨 - 조건
+		if (container != nullptr) {
+			delete[] container;
+		}
+
+		//6. 기존에 배열을 가리키던 포인터 변수의 값을 새로운 배열의 시작 주소로 가리킵니다.
+		container = newcontainer;
+	}
+
+	void PushBack(T data) {
+		if (capacity <= 0) {
+			Resize(1);
+		}
+		else if (size >= capacity) {
+			Resize(capacity * 2);
+		}
+		//크기 할당 후 데이터 삽입
+		container[size++] = data;
+	}
+
+	T& operator[] (const int& index) {//값 변경 X도록 const
+		return container[index];
+	}
+
+	int& Size() {
+		return size;
+	}
+
+	~Vector() {
+		if (container != nullptr) {
+			delete[] container;
 		}
 	}
 
-	void Push(T data) {
-		if (front == rear) { //데이터 0개
-
-			if (rear >= SIZE - 1) {
-				rear = 0;
-				container[0] = data;
-			}
-		}
-		else {
-
-		}
-	}
-
-	void Pop() {
-		if (front >= SIZE -1) {
-			front = 0;
-			container[front] = NULL;
-		}
-		else {
-			container[++front] = NULL;
-		}
-	}
-
-
-	void Show() {
-		for (int i = 0;i < SIZE;i++) {
-			cout << container[i] << endl;
-		}
-	}
 };
 
 int main()
 {
-	CircleQueue<int> circleQueue;
-	circleQueue.Push(10);
-	circleQueue.Pop();
-	circleQueue.Pop();
+	Vector<int> vector;
+	vector.PushBack(10);
+	vector.PushBack(20);
+	vector.PushBack(30);
 
-	circleQueue.Show();
+	for (int i = 0; i < vector.Size();i++) {
+		cout << vector[i] << endl;
+	}
 
 	return 0;
 }
